9411a185032e9a9295f28cff5527a0abef7b22cf
AleHts29 <ahuertas@fi.uba.ar>
2021-04-22 23:33:20 -0300

    entrega clase09

---
+++ clase09/js/main.js
@@ -0,0 +1,38 @@
+//Se aplican eventos a un formulario
+//Se valida que el mail sea valido
+//Se envia el formulario al servidor
+
+
+
+
+const contenedor = document.querySelector(".flex-container");
+const boton = document.querySelector(".send-button");
+boton.value = boton.value.toUpperCase();
+
+function crearProducto(nombre, id, precio){
+    img = "<img class='producto-img' src='img/producto.png'>";
+    nombre = `<h2>${nombre}</h2>`;
+    id = `<h3>${id}</h3>`;
+    precio = `<p>Precio: <b>$${precio}</b></p>`;
+    return [img, nombre, id, precio];
+}
+
+const changeHidden = (number)=>{
+    document.querySelector(".key-data").value = number;
+}
+
+let documentFragment = document.createDocumentFragment();
+
+for (var i = 1; i <= 15; i++){
+    let idRandom = Math.round( Math.random()*10000);
+    let precioRandom = Math.round( Math.random()*10+30);
+    let producto = crearProducto(`Curso ${i}`, `ID ${idRandom}`, `${precioRandom} USD`);
+    let div = document.createElement("DIV");
+    div.addEventListener("click", () => {changeHidden(idRandom)});
+    div.tabIndex = i;
+    div.classList.add(`ìtem-${i}`,'flex-item');
+    div.innerHTML = producto[0] + producto[1] + producto[2] + producto[3];
+    documentFragment.appendChild(div);
+}
+
+contenedor.appendChild(documentFragment);
