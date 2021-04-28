348a2cc9b0ba47e27f274d05680b5f5219b5dffd
AleHts29 <ahuertas@fi.uba.ar>
2021-04-28 18:37:43 -0300

    2da Entrega proyecto

---
+++ 2da_entrega_PF/js/pedido.js
@@ -0,0 +1,25 @@
+const carro = new Carrito();
+
+const carrito = document.getElementById('carrito');
+
+// Contiene el catalogo de productos
+const productos = document.getElementById('lista-productos');
+
+// Accedemos a la lista de productos(tbody), donde vamos a insertar los elementos HTML
+const listaProductos = document.querySelector('#lista-carrito tbody');
+
+const vaciarCarritoBtn = document.getElementById('vaciar-carrito');
+
+
+cargarEventos()
+
+
+function cargarEventos(){
+    productos.addEventListener('click',(e)=>{carro.comprarProducto(e)});
+
+    carrito.addEventListener('click', (e)=>{carro.eliminarProducto(e)});
+
+    vaciarCarritoBtn.addEventListener('click', (e) => {carro.vaciarCarrito(e)});
+
+    document.addEventListener('DOMContentLoaded', carro.leerLocalStorage());
+}
