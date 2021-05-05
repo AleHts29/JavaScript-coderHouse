80e851eb3e7df871954eaf8b3871b4e4f2c66b04
AleHts29 <ahuertas@fi.uba.ar>
2021-05-05 12:52:21 -0300

    clase12

--- clase12/js/pedido.js
+++ clase12/js/pedido.js
@@ -1,29 +1,29 @@
 const carro = new Carrito();
 
-const carrito = document.getElementById('carrito');
+const carrito = $('#carrito');
 
 // Contiene el catalogo de productos
-const productos = document.getElementById('lista-productos');
+const productos = $('#lista-productos');
 
 // Accedemos a la lista de productos(tbody), donde vamos a insertar los elementos HTML
 const listaProductos = document.querySelector('#lista-carrito tbody');
 
-const vaciarCarritoBtn = document.getElementById('vaciar-carrito');
+const vaciarCarritoBtn = $('#vaciar-carrito');
 
-const procesarPedidoBtn = document.getElementById('procesar-pedido');
+const procesarPedidoBtn = $('#procesar-pedido');
 
 
 cargarEventos()
 
 
 function cargarEventos(){
-    productos.addEventListener('click',(e)=>{carro.comprarProducto(e)});
+    productos.click((e)=>{carro.comprarProducto(e)});
 
-    carrito.addEventListener('click', (e)=>{carro.eliminarProducto(e)});
+    carrito.click((e)=>{carro.eliminarProducto(e)});
 
-    vaciarCarritoBtn.addEventListener('click', (e) => {carro.vaciarCarrito(e)});
+    vaciarCarritoBtn.click( (e) => {carro.vaciarCarrito(e)});
 
     document.addEventListener('DOMContentLoaded', carro.leerLocalStorage());
 
-    procesarPedidoBtn.addEventListener('click', (e)=>{carro.procesarPedido(e)});
+    procesarPedidoBtn.click( (e)=>{carro.procesarPedido(e)});
 }
