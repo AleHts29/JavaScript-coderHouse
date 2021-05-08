a1d0f543277b9db6743b17c89ffcd5f1b7b1d308
AleHts29 <ahuertas@fi.uba.ar>
2021-05-08 02:53:42 -0300

    clase13

--- clase13/js/eventos.js
+++ clase13/js/eventos.js
@@ -1,3 +1,13 @@
 // Intro - bienvenida
 let welcome = $(".pricing-header");
 welcome.show(2000);
+
+// Carts
+$("#lista-productos").fadeIn(2500);
+$("#lista-productos").delay(100).fadeOut(1500, function(){
+    $("#lista-productos").fadeIn(1000);
+    $("body").fadeIn(1000).css("background-color", "#F9E79F");
+    welcome.fadeIn(1000).css("color","black");
+});
+
+
